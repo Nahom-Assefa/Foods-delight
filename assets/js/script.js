dbe558b374050661f959c56f1f04ea599cb5144f
Nahom Assefa <nzenebe@gmail.com>
2022-02-13 16:33:03 -0600

    variable declarations

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var recipeApiKey = "b49c94e39bb5414b2d07ad05bd3ec5f2"	
+let imageApiKey ="RUQuxPjGR6Ilkox7XXVJeDgtrx5S3OXASTUxQRUKq3w"
+var searchButton = document.querySelector("#search-btn")
+var result = document.querySelector("#result")
+let resultContainer = document.getElementById("result-container")
+let imageOne = document.getElementById("image-1")
+let imageTwo = document.getElementById("image-2")
