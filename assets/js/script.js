20e45938a2bbd1ab1eb95b8801cc4ba59247523f
Nahom Assefa <nzenebe@gmail.com>
2022-02-14 09:48:34 -0600

    develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ function searchRecipe() {
     })
     .then(function (data) {
       searchedResult(data);
+      console.log("RecipeAPI", data);
     });
   var imageApi =
     "https://api.unsplash.com/search/photos?page=1&query=" +
@@ -31,78 +32,77 @@ function searchRecipe() {
     imageApiKey;
 
   fetch(imageApi)
-    .then(function (response) {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
     })
-    .then(function (data2) {
+    .then((data2) => {
       searchedImages(data2);
+      console.log("imageAPI", data2);
     });
 }
 
-function searchedResult(data){
-    resultContainer.innerHTML=""
-let title = document.createElement("h4")
-title.innerText = data.hits[0].recipe.label
-title.setAttribute("class" ,"title")
-resultContainer.appendChild(title)
+function searchedResult(data) {
+  resultContainer.innerHTML = "";
+  let title = document.createElement("h4");
+  title.innerText = data.hits[0].recipe.label;
+  title.setAttribute("class", "title");
+  resultContainer.appendChild(title);
 
-let ingredientLines = document.createElement("ul")
+  let ingredientLines = document.createElement("ul");
 
-resultContainer.appendChild(ingredientLines)
+  resultContainer.appendChild(ingredientLines);
 
-let lists = data.hits[0].recipe.ingredientLines
-for (let i = 0; i < lists.length; i++) {
-    let ingredientList = document.createElement("li")
-    ingredientList.setAttribute("class" ,"ingredientList")
+  let lists = data.hits[0].recipe.ingredientLines;
+  for (let i = 0; i < lists.length; i++) {
+    let ingredientList = document.createElement("li");
+    ingredientList.setAttribute("class", "ingredientList");
 
-    ingredientLines.appendChild(ingredientList)
-    ingredientList.innerText= lists[i]
+    ingredientLines.appendChild(ingredientList);
+    ingredientList.innerText = lists[i];
+  }
+
+  let source = document.createElement("p");
+  source.innerText = "Source: " + data.hits[0].recipe.source;
+  resultContainer.appendChild(source);
+  var aTag = document.createElement("a");
+  aTag.setAttribute("href", data.hits[0].recipe.url);
+  aTag.innerHTML = data.hits[0].recipe.url;
+  resultContainer.appendChild(aTag);
+
+  let titleTwo = document.createElement("h4");
+  titleTwo.innerText = data.hits[3].recipe.label;
+  titleTwo.setAttribute("class", "title");
+  resultContainer.appendChild(titleTwo);
+
+  let ingredientLine = document.createElement("ul");
+
+  resultContainer.appendChild(ingredientLine);
+
+  let list = data.hits[3].recipe.ingredientLines;
+  for (let i = 0; i < list.length; i++) {
+    let ingredientLists = document.createElement("li");
+    ingredientLists.setAttribute("class", "ingredientList");
+
+    ingredientLine.appendChild(ingredientLists);
+    ingredientLists.innerText = list[i];
+  }
+
+  let sources = document.createElement("p");
+  sources.innerText = "Source: " + data.hits[3].recipe.source;
+  resultContainer.appendChild(sources);
+  var aTag = document.createElement("a");
+  aTag.setAttribute("href", data.hits[3].recipe.url);
+  aTag.innerHTML = data.hits[3].recipe.url;
+  resultContainer.appendChild(aTag);
 }
 
-let source = document.createElement("p")
-source.innerText = "Source: " + data.hits[0].recipe.source
-resultContainer.appendChild(source)
-var aTag = document.createElement('a')
-aTag.setAttribute('href', data.hits[0].recipe.url)
-aTag.innerHTML = data.hits[0].recipe.url
-resultContainer.appendChild(aTag)
+function searchedImages(data2) {
+  firstImage = data2.results[0].urls.small;
+  imageOne.innerHTML = "<img src='" + firstImage + "'>";
+  secondImage = data2.results[1].urls.small;
 
-let titleTwo = document.createElement("h4")
-titleTwo.innerText = data.hits[3].recipe.label
-titleTwo.setAttribute("class" ,"title")
-resultContainer.appendChild(titleTwo)
-
-let  ingredientLine = document.createElement("ul")
-
-resultContainer.appendChild(ingredientLine)
-
-let list = data.hits[3].recipe.ingredientLines
-for (let i = 0; i < list.length; i++) {
-    let ingredientLists = document.createElement("li")
-    ingredientLists.setAttribute("class" ,"ingredientList")
-
-    ingredientLine.appendChild(ingredientLists)
-    ingredientLists.innerText= list[i]
+  imageTwo.innerHTML = "<img src='" + secondImage + "'>";
 }
-
-let sources = document.createElement("p")
-sources.innerText = "Source: " + data.hits[3].recipe.source
-resultContainer.appendChild(sources)
-var aTag = document.createElement('a')
-aTag.setAttribute('href', data.hits[3].recipe.url)
-aTag.innerHTML = data.hits[3].recipe.url
-resultContainer.appendChild(aTag)
-}
-
-function searchedImages(data2){
-    firstImage = data2.results[0].urls.small
-    imageOne.innerHTML=("<img src='" + firstImage  + "'>")
-    secondImage = data2.results[1].urls.small
-    
-    imageTwo.innerHTML=("<img src='" + secondImage  + "'>")
-    
-    }
-    searchButton.addEventListener("click",searchRecipe )
-
+searchButton.addEventListener("click", searchRecipe);
